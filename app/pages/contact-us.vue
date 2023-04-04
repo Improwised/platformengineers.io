b123ace391d829bd37cc002938defede9fe484c2
tapanbavaliya <tbavaliya@gmail.com>
2023-04-04 19:01:53 +0530

    added contact us page

--- app/pages/contact-us.vue
+++ app/pages/contact-us.vue
@@ -1,34 +1,25 @@
 <template>
   <div class="main-container">
-    <section class="text-center">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-lg-6">
-            <h1>Let's talk about your project</h1>
-            <p class="lead">
-              Visit us or make contact via the form below to talk more about
-              your project, ideas or any others technical queries.
-            </p>
+    <div class="contact-us-bk">
+      <Navigation />
+      <section class="text-center">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8 col-lg-6">
+              <h1>Let's talk about your project</h1>
+              <p class="lead">
+                Visit us or make contact via the form below to talk more about
+                your project, ideas or any others technical queries.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-    <section class="bg--secondary">
+      </section>
+    </div>
+    <section class="bg-- contact-us-form-bk">
       <div class="container">
         <div class="row justify-content-between">
-          <div class="col-md-5">
-            <h2>Platform Engineers</h2>
-            <h3 class="fs-24">
-              110 - Pride Square, <br />
-              Pushkardham Main Road, Nr. J. K. Chowk, <br />
-              Rajkot - 360005, Gujarat, India.
-            </h3>
-            <p class="lead">
-              We try to respond to all enquiries within 1 business day.
-            </p>
-            <p class="lead">We are open from 10am — 7pm on weekdays.</p>
-          </div>
-          <div class="col-md-6 col-12">
+          <div class="col-md-6 offset-md-3 bs-1 p-5 br-30">
             <div id="errorMessages"></div>
             <div id="success">
               <div class="success-msg">
@@ -46,7 +37,7 @@
                 id="sitekey"
                 type="hidden"
                 name="sitekey"
-                value="6LfM73IUAAAAAFFFAv5jHjpT_UIjRZqD2xcijnwn"
+                value="6Lfe5kwlAAAAAIMs9H9qX2E9X7qdK3PbdRwP6iFg"
               />
               <div class="col-xs-12">
                 <label for="name">Your Name:</label>
@@ -93,6 +84,9 @@
                 </button>
               </div>
             </form>
+            <p class="text-center mt-5 mb-0">
+              We try to respond to all enquiries within 1 business day.
+            </p>
           </div>
         </div>
       </div>
@@ -100,7 +94,12 @@
   </div>
 </template>
 <script>
+import Navigation from "@/components/Navigation.vue";
+
 export default {
+  components: {
+    Navigation,
+  },
   layout: "theme",
   head() {
     const contactTitle = "Contact - Improwised Technologies Pvt. Ltd.";
@@ -179,3 +178,56 @@ export default {
   },
 };
 </script>
+
+<style>
+.contact-us-bk {
+  z-index: 1;
+  overflow: hidden;
+  position: relative;
+  border-radius: 0 0 200px 200px;
+  box-shadow: rgba(0, 0, 0, 0.15) 0 5px 15px 0;
+}
+
+.contact-us-bk::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.8;
+  z-index: -1;
+  background: url("/img/a112.jpg");
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-position: center;
+}
+
+.contact-us-form-bk {
+  overflow: hidden;
+  position: relative;
+}
+
+.contact-us-form-bk::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.09;
+  z-index: -1;
+  background: url("/img/a2.jpg");
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-position: center;
+}
+
+.bs-1 {
+  box-shadow: rgba(0, 0, 0, 0.15) 0 5px 15px 0;
+}
+
+.br-30 {
+  border-radius: 30px;
+}
+</style>
