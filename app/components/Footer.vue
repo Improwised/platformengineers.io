f2061497ceb7e613f5b31d1c82156e758cb4d02a
tapanbavaliya <tbavaliya@gmail.com>
2023-04-04 20:51:07 +0530

    updates footer with services list

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="space--sm footer-2 bg--dark">
     <div class="container">
       <div class="row">
-        <div class="col-md-6 col-lg-4 col-6">
+        <div class="col-md-3 col-sm-6 col-12">
           <h6 class="type--uppercase">Company</h6>
           <ul class="list--hover">
             <li class="">
@@ -10,7 +10,15 @@
             </li>
           </ul>
         </div>
-        <div class="col-md-6 col-lg-4 col-6">
+        <div class="col-md-3 col-sm-6 col-12">
+          <h6 class="type--uppercase">Services</h6>
+          <ul class="list--hover">
+            <li v-for="(service, index) in $store.state.services" :key="index">
+              <a :href="`/services/${service.slug}`">{{ service.title }}</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-3 col-sm-6 col-12">
           <h6 class="type--uppercase">Support</h6>
           <ul class="list--hover">
             <li>
@@ -23,15 +31,15 @@
             </li>
           </ul>
         </div>
-        <div class="col-md-6 col-lg-4 col-6">
+        <div class="col-md-3 col-sm-6 col-12">
           <h6 class="type--uppercase">Resources</h6>
           <ul class="list--hover">
-            <!--             <li class="">
+            <!-- <li class="">
               <a href="/blogs">Blogs</a>
-            </li>
+            </li> -->
             <li class="">
               <a href="/case-studies">Case Studies</a>
-            </li> -->
+            </li>
           </ul>
         </div>
       </div>
