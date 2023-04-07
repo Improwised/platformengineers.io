31d7959a94c036e491b97410be9fe1e5f58d37c5
tapanbavaliya <tapanbavaliya@users.noreply.github.com>
2023-04-07 09:50:05 +0000

    deploy: 31ef2955e27fa06659fc128eb9f2d906f29d4933

--- _nuxt/2699e99.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{458:function(n,t,e){"use strict";e.r(t);var r={asyncData:function(n){return(0,n.redirect)("/")}},c=e(45),component=Object(c.a)(r,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
-//# sourceMappingURL=2699e99.js.map
