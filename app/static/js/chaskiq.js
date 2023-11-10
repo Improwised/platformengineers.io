f0234159b6d2fd65bd444a4dc05c7a53ee99235a
Payal Raviya <87559461+payalraviya@users.noreply.github.com>
2023-11-10 15:42:34 +0530

    fix: live chat button position (#42)
    * fix: chat button position
    
    * fix: remove unwanted code

--- app/static/js/chaskiq.js
+++ app/static/js/chaskiq.js
@@ -4,6 +4,14 @@
   g.src = "https://support.platformengineers.io/embed.js";
   s.parentNode.insertBefore(g, s);
   g.onload = function () {
+    var currentRoute = window.location.pathname;
+
+    var right = currentRoute === "/contact-us" ? "60px" : "";
+
+    var style = document.createElement("style");
+    style.innerHTML = "#chaskiqPrime { right: " + right + " !important; }";
+    document.head.appendChild(style);
+
     new window.ChaskiqMessengerEncrypted({
       domain: "https://support.platformengineers.io",
       ws: "wss://support.platformengineers.io/cable",
