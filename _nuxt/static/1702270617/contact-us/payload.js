7a7dddc2926ba5444267e9f5436a9149a8eb28de
tapanbavaliya <tapanbavaliya@users.noreply.github.com>
2023-12-12 15:01:53 +0000

    deploy: 0611cf2640ba05c0465737584e9e736bd3a5c002

--- _nuxt/static/1702270617/contact-us/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/contact-us", {data:[{_img:{"/_ipx/h_55,f_png/img/plateform-engineers.png":"\u002F_nuxt\u002Fimage\u002F8686bc.png","/_ipx/f_png/img/contact-us.png":"\u002F_nuxt\u002Fimage\u002F398fa7.png"}}],fetch:{},mutations:[]});
