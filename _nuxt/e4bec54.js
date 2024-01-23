837f09edae751fea58969854193c73741c7de4f8
payalraviya <payalraviya@users.noreply.github.com>
2024-01-23 11:49:18 +0000

    deploy: 4710e124773ecd97c9436d65cbd91426c1528fb6

--- _nuxt/e4bec54.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{487:function(n,t,e){"use strict";e.r(t);var r={asyncData:function(n){return(0,n.redirect)("/")}},c=e(45),component=Object(c.a)(r,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
-//# sourceMappingURL=e4bec54.js.map
