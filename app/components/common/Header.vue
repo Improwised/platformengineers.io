8310590acb1ad1ef64fa527126ec8a2b365f83a1
Payal17122000 <raviyapayal17@gmail.com>
2024-01-24 12:00:34 +0530

    fix: remove extra divs

--- app/components/common/Header.vue
+++ app/components/common/Header.vue
@@ -1,36 +1,29 @@
 <template>
-  <div>
-    <div class="container">
-      <div class="row py-2">
-        <div class="col-md-6 d-flex align-items-center justify-content-center">
-          <div>
-            <div>
-              <h2>{{ title }}</h2>
-              <p v-if="description" class="lead">
-                {{ description }}
-              </p>
-              <div v-if="contactus">
-                <a
-                  class="btn btn--primary type--uppercase b-30"
-                  href="/contact-us"
-                >
-                  <span class="btn__text"> Contact Us </span>
-                </a>
-              </div>
-            </div>
+  <div class="container">
+    <div class="row py-2">
+      <div class="col-md-6 d-flex align-items-center justify-content-center">
+        <div>
+          <h2>{{ title }}</h2>
+          <p v-if="description" class="lead">
+            {{ description }}
+          </p>
+          <div v-if="contactus">
+            <a class="btn btn--primary type--uppercase b-30" href="/contact-us">
+              <span class="btn__text"> Contact Us </span>
+            </a>
           </div>
         </div>
-        <div class="col-md-6 d-flex align-items-center justify-content-center">
-          <nuxt-img
-            v-if="image"
-            :src="image"
-            :alt="`${title} | Platform Engineers`"
-            format="png"
-            loading="lazy"
-            height="400px"
-            class="img-dim"
-          />
-        </div>
+      </div>
+      <div class="col-md-6 d-flex align-items-center justify-content-center">
+        <nuxt-img
+          v-if="image"
+          :src="image"
+          :alt="`${title} | Platform Engineers`"
+          format="png"
+          loading="lazy"
+          height="400px"
+          class="img-dim"
+        />
       </div>
     </div>
   </div>
