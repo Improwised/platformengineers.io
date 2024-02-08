d26b52f44cd9d0c54b82d650f2179859a9d4f0b6
monikagorkhiya <monikagorkhiya@users.noreply.github.com>
2024-02-08 12:21:17 +0000

    deploy: acdd4ff6db0dfc86a403a409d953f1067ec45a2f

--- cssBundel.js
+++ cssBundel.js
@@ -2,7 +2,7 @@ require("./theme/css/bootstrap.css");
 require("./theme/css/stack-interface.css");
 require("./theme/css/socicon.css");
 // require("./theme/css/lightbox.min.css");
-require("./theme/css/flickity.css");
+// require("./theme/css/flickity.css");
 require("./theme/css/iconsmind.css");
 // require("./theme/css/jquery.steps.css");
 require("./theme/css/theme.css");
