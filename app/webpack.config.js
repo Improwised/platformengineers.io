71723ad9ed08ba17fb5ccd53366d07538610d02d
Monika Gorkhiya <97655102+monikagorkhiya@users.noreply.github.com>
2024-02-27 19:31:01 +0530

    Revert "Lighthouse Performance : add purgecss integration in webpack" (#155)

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,9 +1,6 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const PurgeCSSPlugin = require("purgecss-webpack-plugin"); // Add this line
-const glob = require("glob-all"); // Add this line
-
 
 module.exports = {
   mode: "production",
@@ -62,17 +59,6 @@ module.exports = {
   plugins: [
     new MiniCssExtractPlugin({
       filename: "index.min.css",
-    }),
-    new PurgeCSSPlugin({
-      paths: glob.sync([
-        path.join(__dirname, './layouts/**/*.vue'),
-        path.join(__dirname, './components/**/*.vue'),
-        path.join(__dirname, './pages/**/*.vue'),
-        path.join(__dirname, './plugins/**/*.js'),
-      
-      ]),
-      styleExtensions: ['.css'],
-      whitelist: ["html", "body"],
     }),
   ],
 };
