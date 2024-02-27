a6a10e688ef288b06f951b09e9db183e7122d7ac
monikagorkhiya <monika@improwised.com>
2024-02-27 14:14:50 +0530

    footer - Lists do not contain only <li> elements and script supporting elements issue fix

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -4,39 +4,39 @@
       <div class="row">
         <div class="col-md-3 col-sm-6 col-12 mb-5">
           <h3 class="type--uppercase m-0 h6">Company</h3>
-          <ul class="list--hover">
+          <ul class="list--hover mb-1">
             <li class="">
               <a href="/about-us/">About Us</a>
             </li>
-            <div class="d-flex mt-2 icons">
-              <li>
-                <a
-                  href="https://www.facebook.com/profile.php?id=61555276800801"
-                  title="Platform Engineers facebook page"
-                  target="_blank"
-                >
-                  <i class="socicon socicon-facebook icon icon--xs"></i>
-                </a>
-              </li>
-              <li class="px-2">
-                <a
-                  href="https://twitter.com/platform_engr"
-                  title="Platform Engineers twitter account"
-                  target="_blank"
-                >
-                  <i class="socicon socicon-twitter icon icon--xs"></i>
-                </a>
-              </li>
-              <li class="px-1">
-                <a
-                  href="https://www.linkedin.com/company/platform-engineers/mycompany/"
-                  title="Platform Engineers linkedin page"
-                  target="_blank"
-                >
-                  <i class="socicon socicon-linkedin icon icon--xs"></i>
-                </a>
-              </li>
-            </div>
+          </ul>
+          <ul class="list--hover d-flex mt-2 icons">
+            <li>
+              <a
+                href="https://www.facebook.com/profile.php?id=61555276800801"
+                title="Platform Engineers facebook page"
+                target="_blank"
+              >
+                <i class="socicon socicon-facebook icon icon--xs"></i>
+              </a>
+            </li>
+            <li class="px-2">
+              <a
+                href="https://twitter.com/platform_engr"
+                title="Platform Engineers twitter account"
+                target="_blank"
+              >
+                <i class="socicon socicon-twitter icon icon--xs"></i>
+              </a>
+            </li>
+            <li class="px-1">
+              <a
+                href="https://www.linkedin.com/company/platform-engineers/mycompany/"
+                title="Platform Engineers linkedin page"
+                target="_blank"
+              >
+                <i class="socicon socicon-linkedin icon icon--xs"></i>
+              </a>
+            </li>
           </ul>
           <!--  -->
           <h3 class="type--uppercase m-0 h6">Resources</h3>
@@ -64,7 +64,9 @@
               v-for="(industry, index) in $store.state.industries"
               :key="index"
             >
-              <a :href="`/industries/${industry.slug}/`">{{ industry.title }}</a>
+              <a :href="`/industries/${industry.slug}/`">{{
+                industry.title
+              }}</a>
             </li>
           </ul>
         </div>
