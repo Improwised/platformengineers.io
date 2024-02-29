7f7eea7610f8951b5204f3c48d6c560558a58574
monikagorkhiya <monika@improwised.com>
2024-02-29 15:40:24 +0530

    solve webpack js,css extention error

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "./static/dist"),
-    filename: "[name].min.css",
+    filename: "[name].min.js",
   },
   optimization: {
     splitChunks: {
